065c0d2e2b56b5dddfa59a8098894e69a8ee7d74
Matt Kline <matt@bitbashing.io>
2025-03-23 17:57:10 -0700

    Rust 1.85.1

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@ in rec
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [
-    (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/c932b3873a5d56126bc1f1416fb8a58315f86c17.tar.gz"))
+    (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/01973c84732f9275c50c5f075dd1f54cc04b3316.tar.gz"))
     (self: super: { mrkline = import ./overlay/packages.nix { inherit (self) config pkgs lib; }; })
   ];
 
