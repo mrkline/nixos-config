4e3bf5e2423d9ed93b19bd6a0796b9d171b1ba5f
Matt Kline <mkline@anduril.com>
2026-01-08 13:32:06 -0800

    UTM: Rosetta works now

--- utm-hardware-configuration.nix
+++ utm-hardware-configuration.nix
@@ -75,6 +75,8 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 
+  virtualisation.rosetta.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
