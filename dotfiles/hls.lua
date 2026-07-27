3c62e9f819a89268b501aad3829e471c8a1416d4
Matt Kline <matt@bitbashing.io>
2026-07-26 18:35:58 -0700

    Fixup HLS detection noise

--- dotfiles/hls.lua
+++ dotfiles/hls.lua
@@ -1,5 +1,5 @@
 function whichHls()
-    handle = io.popen("which haskell-language-server")
+    handle = io.popen("which haskell-language-server 2>/dev/null")
     which = handle:read("*a")
     which = which:gsub('%s+', '')
     io.close(handle)
